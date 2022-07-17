c31a58669edfb06f02385f5d8fc9cc18f74b8643
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-16 22:55:54 -0400

    feat: move hamburger icon in separate file

---
+++ components/Navbar/BarHamburger.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './header.module.css';
+
+const BarHamburger = () => {
+  return (
+    <div className={style.burger}>
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
+
+export default BarHamburger
