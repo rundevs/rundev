d0efe34c373e15a9d6c0b914b12f07266f8801bc
Luis Gabriel Janco <janco7249@gmail.com>
2022-07-17 17:20:50 -0400

    feat: set context for handle themes correctly

---
+++ context/helpers.js
@@ -0,0 +1,44 @@
+const isServer = typeof window === 'undefined'
+const MEDIA = '(prefers-color-scheme: dark)'
+/**
+ * @param {string} key
+ * @returns {string | any}
+ */
+export const getTheme = (key, fallback) => {
+  if (isServer) return undefined
+  let theme
+  try {
+    theme = localStorage.getItem(key) || undefined
+  } catch (e) {
+    // Unsupported
+  }
+  return theme || fallback
+}
+
+export const disableAnimation = () => {
+  const css = document.createElement('style')
+  css.appendChild(
+    document.createTextNode(
+      `*{-webkit-transition:none!important;-moz-transition:none!important;-o-transition:none!important;-ms-transition:none!important;transition:none!important}`
+    )
+  )
+  document.head.appendChild(css)
+
+  return () => {
+    // Force restyle
+    ; (() => window.getComputedStyle(document.body))()
+
+    // Wait for next tick before removing
+    setTimeout(() => {
+      document.head.removeChild(css)
+    }, 1)
+  }
+}
+
+export const getSystemTheme = (e) => {
+  if (!e) e = window.matchMedia(MEDIA)
+  const isDark = e.matches
+  const systemTheme = isDark ? 'dark' : 'light'
+  return systemTheme
+}
+
