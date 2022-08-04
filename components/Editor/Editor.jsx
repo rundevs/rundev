0de245b0a040e5886d9d294b5ea7bf7d495859bc
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-03 22:31:15 -0400

    feat: setup editor and preview component

---
+++ components/Editor/Editor.jsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useRef } from 'react'
+import EditorCode from '@monaco-editor/react'
+import useThemes from '../../hooks/useThemes'
+// import onedark from './onedark.json'
+import style from './editor.module.css'
+
+/**
+ * @param {{initialDoc: string, onChange: (newDoc: string) => void}}
+ * @returns {JSX.Element}
+ */
+const Editor = ({ initialDoc, onChange }) => {
+  const handleChange = useCallback((value) => onChange(value), [onChange])
+  const editorRef = useRef(null)
+  const { theme, checkIfIsDark } = useThemes()
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor
+    console.log(monaco)
+    // monaco.editor.defineTheme("onedark", onedark)
+    // monaco.editor.setTheme("onedark")
+  }
+
+  // const handleSave = () => {
+  //   // const content = editorRef.current.getValue()
+  // }
+
+  return (
+    <EditorCode
+      value={initialDoc}
+      theme={checkIfIsDark() ? 'vs-dark' : 'vs'}
+      language='markdown'
+      onChange={handleChange}
+      className={style.editor}
+      onMount={handleEditorDidMount}
+    />
+  )
+}
+
+export default Editor
---
+++ components/Preview/Preview.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './preview.module.css'
+
+const Preview = () => {
+  return (
+    <article className={style.preview}>Preview</article>
+  )
+}
+
+export default Preview
