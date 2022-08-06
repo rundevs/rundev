a7e3f009178bd77a3f7a7fc73311e6ae611a358d
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-06 19:52:54 -0400

    feat: add footer to show details of editor

--- pages/app.js
+++ pages/app.js
@@ -1,67 +1,99 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { Fragment, useCallback, useEffect, useState } from 'react'
 import Split from 'react-split-grid'
+import Head from 'next/head'
 import Editor from '../components/Editor/Editor'
 import NavEditor from '../components/Navbar/NavEditor/NavEditor'
 import Preview from '../components/Preview/Preview'
 import style from '../styles/app.module.css'
+import useMatchQuery from '../hooks/useMatchQuery'
+import useClient from '../hooks/useClient'
 
-/** @todo move state matched in custom hook */
 const App = () => {
   const [doc, setDoc] = useState(INITIAL_STATE)
-  const [matched, setMatched] = useState(false)
+  const { matched } = useMatchQuery()
+  const { mounted } = useClient()
 
   const handleChange = useCallback((/** @type string */ newDoc) => {
     setDoc(newDoc)
   }, [])
 
-  const matchQuery = useCallback((query) => {
-    const gridElement = document.getElementById('grid')
-    if (query.matches) {
-      setMatched(true)
-      gridElement.style.gridTemplateColumns = '1fr 8px 1fr'
-      gridElement.style.gridTemplateRows = '1fr'
-    } else {
-      setMatched(false)
-      gridElement.style.gridTemplateRows = '1fr 8px 1fr'
-      gridElement.style.gridTemplateColumns = '1fr'
+  let subscribe = true
+  useEffect(() => {
+    if (subscribe) {
+      document.body.style.overflowY = 'hidden'
+      document.querySelector('html').style.position = 'fixed'
+    }
+    return () => {
+      subscribe = false
     }
   }, [])
 
-  const mediaQuery = typeof window !== 'undefined' && window.matchMedia('(min-width: 640px)')
-
-  let subscribe = true
-  useEffect(() => {
-    subscribe && matchQuery(mediaQuery)
-    return () => {
-      subscribe = false
-    }
-  }, []);
-
-  useEffect(() => {
-    if (subscribe) {
-      typeof window !== 'undefined' && mediaQuery.addEventListener('change', matchQuery)
-    }
-    return () => {
-      subscribe = false
-    }
-  }, [mediaQuery])
-
   return (
-    <main className={style.app}>
-      <NavEditor />
-      <Split rowMinSize={100} columnMinSize={200} render={({ getGridProps, getGutterProps }) => (
-        <div id='grid' className={matched ? style.gridColumn : style.gridRow} {...getGridProps()}>
-          <div style={{ height: '100%', position: 'relative', overflow: 'hidden' }}>
-            <Editor initialDoc={doc} onChange={handleChange} />
-          </div>
-          <div className={matched ? style.gutterColumn : style.gutterRow} {...getGutterProps(matched ? 'column' : 'row', 1)} />
-          <div style={{ width: '100%', overflow: 'auto', position: 'relative' }}>
-            <Preview doc={doc} />
-          </div>
+    <Fragment>
+      <Head>
+        <title>Markdown with preview | Edit, create, and improve your ideas ✨</title>
+      </Head>
+      <main className={style.app}>
+        <div className={style.appLayout}>
+          <NavEditor />
+          <Split rowMinSize={100} columnMinSize={200} render={({ getGridProps, getGutterProps }) => (
+            <div
+              id='grid'
+              className={matched ? style.gridColumn : style.gridRow}
+              {...getGridProps()}
+            >
+              <div style={{ height: '100%', position: 'relative', overflow: 'hidden' }}>
+                {mounted && <Editor initialDoc={doc} onChange={handleChange} />}
+              </div>
+              <div
+                className={matched ? style.gutterColumn : style.gutterRow}
+                {...getGutterProps(matched ? 'column' : 'row', 1)}
+              />
+              <div style={{ width: '100%', height: '100%', overflow: 'auto', position: 'relative', overflow: 'hidden' }}>
+                <Preview doc={doc} />
+              </div>
+            </div>
+          )}
+          />
         </div>
-      )} />
-    </main>
+        <footer className={style.footer}>
+          <div>
+            <span
+              data-tooltip='Markdown Preview Application'
+              className={style.tooltip}
+            >
+              Markdown Preview
+            </span>
+          </div>
+          <div>
+            <span
+              data-tooltip='Indentation'
+              className={style.tooltip}
+            >
+              Spaces: 2
+            </span>
+            <span
+              data-tooltip='Encoding'
+              className={style.tooltip}
+            >
+              UTF-8
+            </span>
+            <span
+              data-tooltip='Line Sequence'
+              className={style.tooltip}
+            >
+              LF
+            </span>
+            <span
+              data-tooltip='Language'
+              className={style.tooltip}
+            >
+              Markdown
+            </span>
+          </div>
+        </footer>
+      </main>
+    </Fragment>
   )
 }
 
