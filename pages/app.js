8e5ab53ef1a67363e1f5bb078816946f1e3f59ff
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-10 22:58:05 -0400

    feat: configure dynamic rendering for the side panel

--- pages/app.js
+++ pages/app.js
@@ -6,31 +6,26 @@ import Editor from '../components/Playground/Editor/Editor'
 import Footer from '../components/Playground/footer/Footer'
 import Preview from '../components/Playground/Preview/Preview'
 import NavEditor from '../components/Playground/NavEditor/NavEditor'
-import Explorer from '../components/Playground/explorer/Explorer'
+import Explorer from '../components/Playground/Panel/Explorer'
 import useMatchQuery from '../hooks/useMatchQuery'
 import useClient from '../hooks/useClient'
 import style from '../styles/app.module.css'
-import Profile from 'components/Playground/Profile'
+import Profile from 'components/Playground/Panel/Profile'
+import useSelectMenu from 'hooks/useSelectMenu'
+import Settings from 'components/Playground/Panel/Settings'
+import SettingProvider from 'context/SettingProvider'
+
+const arrComponents = { explorer: <Explorer />, user: <Profile />, settings: <Settings /> }
 
 const App = () => {
   const [doc, setDoc] = useState(INITIAL_STATE)
+  const { menu, handleSelectMenu } = useSelectMenu()
   const { matched } = useMatchQuery()
   const { mounted } = useClient()
-  const [activeExplorer, setActiveExplorer] = useState(false)
 
   const handleChange = useCallback((/** @type string */ newDoc) => {
     setDoc(newDoc)
   }, [])
-
-  const handleExplorer = () => setActiveExplorer(!activeExplorer)
-
-  useEffect(() => {
-    if (activeExplorer) {
-      document.getElementById('grid-layout').style.gridTemplateColumns = '200px 5px 1fr'
-    } else {
-      document.getElementById('grid-layout').style.gridTemplateColumns = '0px 0px 1fr'
-    }
-  }, [activeExplorer])
 
   let subscribe = true
   useEffect(() => {
@@ -48,42 +43,44 @@ const App = () => {
       <Head>
         <title>Rundev | Markdown preview | Edit, create, and improve your ideas ✨</title>
       </Head>
-      <main className={style.app}>
-        <div className={style.appLayout}>
-          <NavEditor handleExplorer={handleExplorer} />
-          <Split
-            columnMinSize={170}
-            render={({ getGridProps: getGridLayout, getGutterProps: getGutterLayout }) => (
-              <div id='grid-layout' className={style.gridExplorer} {...getGridLayout()}>
-                <div style={{ height: '100%', position: 'relative', overflow: 'hidden' }}>
-                  {activeExplorer && <Explorer />}
-                  {/* {activeExplorer && <Profile />} */}
-                </div>
-                <div className={style.gutterColExplorer} {...getGutterLayout('column', 1)} />
-                <Split rowMinSize={100} columnMinSize={200} render={({ getGridProps, getGutterProps }) => (
-                  <div
-                    id='grid'
-                    className={matched ? style.gridColumn : style.gridRow}
-                    {...getGridProps()}
-                  >
-                    <section className={style.editorContain}>
-                      {mounted && <Editor initialDoc={doc} onChange={handleChange} />}
-                    </section>
-                    <div
-                      className={matched ? style.gutterColumn : style.gutterRow} {...getGutterProps(matched ? 'column' : 'row', 1)}
-                    />
-                    <section className={style.previewContain}>
-                      <Preview doc={doc} />
-                    </section>
+      <SettingProvider>
+
+        <main className={style.app}>
+          <div className={style.appLayout}>
+            <NavEditor handleSelectMenu={handleSelectMenu} />
+            <Split
+              columnMinSize={234}
+              render={({ getGridProps: getGridLayout, getGutterProps: getGutterLayout }) => (
+                <div id='grid-layout' className={style.gridExplorer} {...getGridLayout()}>
+                  <div style={{ height: '100%', position: 'relative', overflow: 'hidden' }}>
+                    {Object.entries(menu).filter(value => value[1]).map(value => arrComponents[value[0]])[0]}
                   </div>
-                )}
-                />
-              </div>
-            )}
-          />
-        </div>
-        <Footer />
-      </main>
+                  <div className={style.gutterColExplorer} {...getGutterLayout('column', 1)} />
+                  <Split rowMinSize={100} columnMinSize={200} render={({ getGridProps, getGutterProps }) => (
+                    <div
+                      id='grid'
+                      className={matched ? style.gridColumn : style.gridRow}
+                      {...getGridProps()}
+                    >
+                      <section className={style.editorContain}>
+                        {mounted && <Editor initialDoc={doc} onChange={handleChange} />}
+                      </section>
+                      <div
+                        className={matched ? style.gutterColumn : style.gutterRow} {...getGutterProps(matched ? 'column' : 'row', 1)}
+                      />
+                      <section className={style.previewContain}>
+                        <Preview doc={doc} />
+                      </section>
+                    </div>
+                  )}
+                  />
+                </div>
+              )}
+            />
+          </div>
+          <Footer />
+        </main>
+      </SettingProvider>
     </Fragment >
   )
 }
