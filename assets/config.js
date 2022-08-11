42807fb72f2a0b14cb1a3ab8b47f59539ccc11b3
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-11 10:32:06 -0400

    feat: add environment variables of auth providers id

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  clientGitHub: process.env.GITHUB_CLIENT_ID || '',
+  clientGitHubSecret: process.env.GITHUB_CLIENT_SECRET || '',
+  clientSecret: process.env.SECRET || ''
+}
