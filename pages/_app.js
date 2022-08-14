ab9ea0d75d28191475cb70babc34a1dded7cc6df
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-14 00:04:16 -0400

    feat: add google search console

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,11 @@
-/* eslint-disable @next/next/inline-script-id */
 import { ThemeProvider } from '../context/ThemeProvider'
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 import { Fragment } from 'react'
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <Fragment>
-      <Script
-        strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script strategy='lazyOnload'>
-        {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {page_path: window.location.pathname,});`}
-      </Script>
       <ThemeProvider forcedTheme={Component.theme || undefined}>
         <SessionProvider session={session}>
           <Component {...pageProps} />
