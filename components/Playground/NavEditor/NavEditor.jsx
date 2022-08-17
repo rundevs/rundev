36b6923af3201a64a3a6c030722f1be76889ad60
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-16 22:52:39 -0400

    feat: add toggle button to active workspaces

--- components/Playground/NavEditor/NavEditor.jsx
+++ components/Playground/NavEditor/NavEditor.jsx
@@ -1,21 +1,17 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { signIn, useSession } from 'next-auth/react'
+import { BsGrid } from 'react-icons/bs'
 
 import { Files, Setting, User } from '../../../assets/icons'
 import style from './naveditor.module.css'
-
-const themesContent = [
-  { value: 'atom-one-dark', label: 'Atom One Dark' },
-  { value: 'atom-one-light', label: 'Atom One Light' },
-  { value: 'github', label: 'GitHub' },
-  { value: 'github-dark-dimmed', label: 'GitHub Dark' }
-]
+import useSettings from 'hooks/useSettings'
 
 const NavEditor = ({ handleSelectMenu }) => {
   const router = useRouter()
   const { status } = useSession()
+  const { settings, updateSettings } = useSettings()
 
   const handleLoginWithGitHub = () => signIn('github')
 
@@ -25,26 +21,29 @@ const NavEditor = ({ handleSelectMenu }) => {
 
   const handleUser = () => status === 'authenticated' ? selectUser() : handleLoginWithGitHub()
 
+  const toggleWorkSpaces = () => updateSettings('workspaces', !settings.workspaces)
+
   return (
     <Fragment>
       <header className={style.headerEditor}>
         <nav className={style.navEditor}>
           <div className={style.actionEditor}>
-            {/* <Link href='/'> */}
-            <button onClick={() => router.push('/')} className={style.tooltip} data-tooltip='Home'>
+            <button onClick={() => router.push('/')} className={style.tooltip} data-tooltip='Home' title='Home'>
               <div />
               <Image src='/rundevs.png' alt='Rundev logo' width={25} height={25} priority={true} />
             </button>
-            {/* </Link> */}
-            <button onClick={selectExplorer} className={style.tooltip} data-tooltip='Explorer'>
+            <button onClick={selectExplorer} className={style.tooltip} data-tooltip='Explorer' title='Explorer'>
               <Files />
+            </button>
+            <button onClick={toggleWorkSpaces} className={style.tooltip} data-tooltip='WorkSpaces' title='WorkSpaces'>
+              <BsGrid />
             </button>
           </div>
           <div className={style.settingEditor}>
-            <button onClick={handleUser} className={style.tooltip} data-tooltip='Account'>
+            <button onClick={handleUser} className={style.tooltip} data-tooltip='Account' title='Account'>
               <User />
             </button>
-            <button className={style.tooltip} onClick={selectSettings} data-tooltip='Manage'>
+            <button className={style.tooltip} onClick={selectSettings} data-tooltip='Manage' title='Manage'>
               <Setting />
             </button>
           </div>
