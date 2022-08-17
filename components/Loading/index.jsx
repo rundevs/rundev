16507e2ea14f84307302c4ca6e5857b56eb2646e
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-16 22:50:13 -0400

    feat: add loading component with logo

---
+++ components/Loading/index.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+import style from './loading.module.css'
+
+const Loading = () => {
+  return (
+    <div className={style.loading}>
+      <figure>
+        <Image src='/rundevs.png' alt='rundevs animation pulse' height={80} width={80} priority={true} quality={100} />
+      </figure>
+    </div>
+  )
+}
+
+export default Loading
