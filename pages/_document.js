fcbc46855352dd57318c905d16ef53a140e7d345
Luis gabriel Janco <janco7249@gmail.com>
2022-09-02 08:48:47 -0400

    fix: set linter using standard eslint

--- pages/_document.js
+++ pages/_document.js
@@ -5,21 +5,22 @@ class MyDocument extends Document {
   constructor(props) {
     super(props)
   }
+
   render() {
     return (
-      <Html lang="en-US">
+      <Html lang='en-US'>
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="theme-color" media="(prefers-color-scheme: light)" content="#1e1e1e" />
-          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1e1e1e" />
+          <meta charSet='utf-8' />
+          <meta name='theme-color' media='(prefers-color-scheme: light)' content='#1e1e1e' />
+          <meta name='theme-color' media='(prefers-color-scheme: dark)' content='#1e1e1e' />
           <meta name='description' content='Rundev is the best and easier editor on the web' />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Rundev" />
-          <meta property="og:description" content="Rundev is the best and easier editor on the web" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="google-site-verification" content="googlee7d298e9e66eecd5.html" />
-          <meta name="google-site-verification" content="nk6ETUfejHtjHnkXEBzgWhOOX_k-m3iSSVxpoEbVzGM" />
+          <meta property='og:type' content='website' />
+          <meta property='og:title' content='Rundev' />
+          <meta property='og:description' content='Rundev is the best and easier editor on the web' />
+          <meta name='twitter:card' content='summary_large_image' />
+          <link rel='icon' href='/favicon.ico' />
+          <meta name='google-site-verification' content='googlee7d298e9e66eecd5.html' />
+          <meta name='google-site-verification' content='nk6ETUfejHtjHnkXEBzgWhOOX_k-m3iSSVxpoEbVzGM' />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -33,7 +34,7 @@ class MyDocument extends Document {
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
-          `,
+          `
             }}
           />
         </Head>
