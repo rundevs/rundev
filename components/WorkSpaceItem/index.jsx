9ae74b6638ca68d6a9756952eae44c259f40651d
Luis gabriel Janco <janco7249@gmail.com>
2022-09-03 23:14:54 -0400

    feat(Root): add new design of item on work spaces

--- components/WorkSpaceItem/index.jsx
+++ components/WorkSpaceItem/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import useSettings from 'hooks/useSettings'
-import style from '../../styles/workspaces.module.css'
 import Link from 'next/link'
 
 const WorkspacesItem = ({ lang, color, label, icon }) => {
@@ -11,9 +10,11 @@ const WorkspacesItem = ({ lang, color, label, icon }) => {
     <Link href={`/${lang}`}>
       <a
         onClick={() => handleLanguage(lang)}
-        className={style.workItemAvailable}
+        style={{ borderColor: color.secondary, backgroundColor: color.primary, boxShadow: `0 0 15px -3px ${color.primary}, 0 0 6px -4px ${color.primary}` }}
+        aria-label={color.secondary}
+        className='flex flex-row items-center gap-5 w-max px-4 py-2 rounded-xl border shadow-none brightness-90 hover:brightness-100 transition-all duration-100 hover:scale-105 text-lg md:text-xl lg:text-2xl lg:py-4 lg:px-8 lg:border-2'
       >
-        <figure style={{ color }}>{icon}</figure>
+        <figure style={{ color: color.secondary }}>{icon}</figure>
         <span>{label}</span>
       </a>
     </Link>
