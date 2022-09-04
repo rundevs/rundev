fafe268b7c148feea07049447d01988ff8cfbd58
Luis gabriel Janco <janco7249@gmail.com>
2022-09-03 23:13:54 -0400

    refactor(Root): add hook to match query for set grid split

--- components/Playground/index.jsx
+++ components/Playground/index.jsx
@@ -8,15 +8,28 @@ import style from './playground.module.css'
 import { useRouter } from 'next/router'
 import { decode, encode } from 'js-base64'
 
+const isDoc = typeof document !== 'undefined'
+
 const Playground = () => {
   const [doc, setDoc] = useState(INITIAL_STATE)
-  const { matched } = useMatchQuery()
+  const matched = useMatchQuery('(min-width: 640px)')
   const router = useRouter()
   const { asPath } = router
 
   useEffect(() => {
     if (asPath.slice(5) !== '') {
       setDoc(decode(asPath.slice(5)))
+    }
+  }, [])
+
+  useEffect(() => {
+    const gridElement = isDoc && document.getElementById('grid')
+    if (matched) {
+      gridElement.style.gridTemplateColumns = '1fr 5px 1fr'
+      gridElement.style.gridTemplateRows = '1fr'
+    } else {
+      gridElement.style.gridTemplateRows = '1fr 5px 1fr'
+      gridElement.style.gridTemplateColumns = '1fr'
     }
   }, [])
 
@@ -28,23 +41,25 @@ const Playground = () => {
   }, [])
 
   return (
-    <Split rowMinSize={100} columnMinSize={200} render={({ getGridProps, getGutterProps }) => (
-      <div
-        id='grid'
-        className={matched ? style.gridColumn : style.gridRow}
-        {...getGridProps()}
-      >
-        <section className={style.editorContain}>
-          <Editor initialDoc={doc} onChange={handleChange} />
-        </section>
+    <Split
+      rowMinSize={100} columnMinSize={200} render={({ getGridProps, getGutterProps }) => (
         <div
-          className={matched ? style.gutterColumn : style.gutterRow} {...getGutterProps(matched ? 'column' : 'row', 1)}
-        />
-        <section className={style.previewContain}>
-          <Preview doc={doc} />
-        </section>
-      </div>
-    )} />
+          id='grid'
+          className={matched ? style.gridColumn : style.gridRow}
+          {...getGridProps()}
+        >
+          <section className={style.editorContain}>
+            <Editor initialDoc={doc} onChange={handleChange} />
+          </section>
+          <div
+            className={matched ? style.gutterColumn : style.gutterRow} {...getGutterProps(matched ? 'column' : 'row', 1)}
+          />
+          <section className={style.previewContain}>
+            <Preview doc={doc} />
+          </section>
+        </div>
+      )}
+    />
   )
 }
 
