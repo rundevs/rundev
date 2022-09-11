84b35e22f9a94b9cfa29150e07142652616fdfbf
Luis gabriel Janco <janco7249@gmail.com>
2022-09-10 20:00:31 -0400

    refactor(Root): separate functionality to reuse in other scopes

--- hooks/usePlatform.js
+++ hooks/usePlatform.js
@@ -1,26 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-function checkPlatform() {
-  if (typeof navigator !== 'undefined') {
-    return [navigator.userAgent.indexOf('Win')].includes(-1)
-      ? [navigator.userAgent.indexOf('Linux')].includes(-1) ? 'Mac' : 'Linux' : 'Windows'
+const existNavigator = typeof navigator !== 'undefined'
+
+/** @param {string} os */
+function verifyOs (os) {
+  return [navigator.userAgent.indexOf(os)].includes(-1)
+}
+
+function checkPlatform () {
+  if (existNavigator) {
+    return verifyOs('Win')
+      ? verifyOs('Linux') ? 'Mac' : 'Linux'
+      : 'Windows'
   }
   return 'Platform'
 }
 
-function checkIfMobile() {
-  if (typeof navigator !== 'undefined') {
-    return [navigator.userAgent.indexOf('Mobile')].includes(-1) ? false : true
+function checkIfMobile () {
+  if (existNavigator) {
+    return !verifyOs('Mobile')
   }
   return false
 }
 
-export default function usePlatform() {
+export default function usePlatform () {
   const [platform, setPlatform] = useState('Platform')
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    if (typeof navigator !== 'undefined') {
+    if (existNavigator) {
       setPlatform(checkPlatform())
       setIsMobile(checkIfMobile())
     }
