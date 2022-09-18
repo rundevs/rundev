fbd02772646c64fd35963ae8613000ca0893dd67
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-18 15:54:49 -0400

    refactor(Root): set props to dynamic language

--- components/HighlightCode.jsx
+++ components/HighlightCode.jsx
@@ -1,11 +1,14 @@
 import highlightjs from 'highlight.js'
 import 'highlight.js/styles/atom-one-dark.css'
-// import 'highlight.js/styles/github-dark-dimmed.css'
 
-const HighlightCode = ({ codeString }) => {
+const HighlightCode = ({ codeString, language }) => {
   return (
     <pre className='px-4 pt-3 pb-2 leading-none'>
-      <code dangerouslySetInnerHTML={{ __html: highlightjs.highlight(codeString, { language: 'javascript' }).value }} />
+      <code
+        dangerouslySetInnerHTML={{
+          __html: highlightjs.highlight(codeString, { language }).value
+        }}
+      />
     </pre>
   )
 }
