4f0096ce94d0a3f9481d9c6c213fe5091e206301
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-20 08:58:00 -0400

    fix(Root): move doc context provider to root app.js

--- components/layout/AppLayout.js
+++ components/layout/AppLayout.js
@@ -3,7 +3,6 @@ import NavEditor from 'components/Playground/NavEditor/NavEditor'
 import Explorer from 'components/Playground/Panel/Explorer'
 import Profile from 'components/Playground/Panel/Profile'
 import Settings from 'components/Playground/Panel/Settings'
-import { DocProvider } from 'context/DocProvider'
 import useSelectMenu from 'hooks/useSelectMenu'
 import React, { useEffect } from 'react'
 import Split from 'react-split-grid'
@@ -28,38 +27,36 @@ const AppLayout = ({ children }) => {
   }, [])
 
   return (
-    <DocProvider>
-      <main className='dark:bg-primary bg-white flex flex-col w-full h-full overflow-hidden relative items-center justify-end'>
-        <div className='flex flex-col h-full w-full overflow-hidden relative sm:flex-row'>
-          <NavEditor handleSelectMenu={handleSelectMenu} />
-          <Split
-            columnMinSize={234}
-            render={({
-              getGridProps: getGridLayout,
-              getGutterProps: getGutterLayout
-            }) => (
-              <div
-                id='grid-layout'
-                className='grid-cols-[0px_5px_1fr] h-full w-full grid overflow-hidden'
-                {...getGridLayout()}
-              >
-                <div className='h-full relative overflow-hidden'>
-                  {Object.entries(menu)
-                    .filter((value) => value[1])
-                    .map((value) => arrComponents[value[0]])[0]}
-                </div>
-                <div
-                  className='dark:bg-secondary bg-slate-100 dark:hover:bg-blue-600 dark:active:bg-blue-500 hover:bg-blue-600 active:bg-blue-500 row-[1/-1] cursor-col-resize col-[2] bg-[url("/gutter-col.png")] bg-[50%_center] bg-no-repeat transition-colors duration-200'
-                  {...getGutterLayout('column', 1)}
-                />
-                {children}
+    <main className='dark:bg-primary bg-white flex flex-col w-full h-full overflow-hidden relative items-center justify-end'>
+      <div className='flex flex-col h-full w-full overflow-hidden relative sm:flex-row'>
+        <NavEditor handleSelectMenu={handleSelectMenu} />
+        <Split
+          columnMinSize={234}
+          render={({
+            getGridProps: getGridLayout,
+            getGutterProps: getGutterLayout
+          }) => (
+            <div
+              id='grid-layout'
+              className='grid-cols-[0px_5px_1fr] h-full w-full grid overflow-hidden'
+              {...getGridLayout()}
+            >
+              <div className='h-full relative overflow-hidden'>
+                {Object.entries(menu)
+                  .filter((value) => value[1])
+                  .map((value) => arrComponents[value[0]])[0]}
               </div>
-            )}
-          />
-        </div>
-        <Footer />
-      </main>
-    </DocProvider>
+              <div
+                className='dark:bg-secondary bg-slate-100 dark:hover:bg-blue-600 dark:active:bg-blue-500 hover:bg-blue-600 active:bg-blue-500 row-[1/-1] cursor-col-resize col-[2] bg-[url("/gutter-col.png")] bg-[50%_center] bg-no-repeat transition-colors duration-200'
+                {...getGutterLayout('column', 1)}
+              />
+              {children}
+            </div>
+          )}
+        />
+      </div>
+      <Footer />
+    </main>
   )
 }
 
