f4db43e83897030b9b61477d8ec68b7d547f6fa9
Luis Gabriel Janco <janco7249@gmail.com>
2023-04-17 10:25:10 +0400

    chore(Root): add mdx extension in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
