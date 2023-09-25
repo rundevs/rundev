799d804a9aa4b76f16274a54baf306d18992c727
Luis Gabriel Janco <janco7249@gmail.com>
2023-09-25 19:26:33 -0400

    style: change source files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
