2f8ebb17e30dd225446c0b1f6ac7cc826668e87f
Luis Gabriel Janco <janco7249@gmail.com>
2023-09-25 19:22:39 -0400

    refactor: add environment variables constants

--- src/assets/config.js
+++ src/assets/config.js
@@ -2,5 +2,7 @@
 export default {
   clientGitHub: process.env.GITHUB_CLIENT_ID || '',
   clientGitHubSecret: process.env.GITHUB_CLIENT_SECRET || '',
+  clientGoogle: process.env.GOOGLE_CLIENT_ID || '',
+  clientGoogleSecret: process.env.GOOGLE_CLIENT_SECRET || '',
   clientSecret: process.env.NEXTAUTH_SECRET || ''
 }
